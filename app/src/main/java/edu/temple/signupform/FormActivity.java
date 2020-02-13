bd47a1fea9b326c1b29fb11eeb7f464158daabb1
John <tug52439@temple.edu>
2020-02-12 19:56:41 -0500

    Added Button and OnClick Event

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -2,13 +2,59 @@ package edu.temple.signupform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class FormActivity extends AppCompatActivity {
+public class FormActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+
+        TextView password = findViewById(R.id.textPassword);
+        TextView confirm = findViewById(R.id.textconfirm);
+        TextView email = findViewById(R.id.textEmail);
+        TextView name = findViewById(R.id.textName);
+
+
+        if(email.getText() == "" || password.getText() == "" || confirm.getText() ==""|| name.getText() == ""){
+            Context context = getApplicationContext();
+            CharSequence text = "Please enter all required Data";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else{
+            if(password.getText() != confirm.getText()){
+                Context context = getApplicationContext();
+                CharSequence text = "Passwords Must Match";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+            else{
+                Context context = getApplicationContext();
+                CharSequence text = "Welcome, " + name.getText() + ", to the SignUpForm App.";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        }
+
+
+
     }
 }
